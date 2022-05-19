250664272072fcfbe0d1a7d85c58af692a226fd7
Cyrus Chan <cyruschan1212@gmail.com>
2022-05-19 22:01:27 +0800

    Implement test

--- test/keqv_test.dart
+++ test/keqv_test.dart
@@ -1,3 +1,16 @@
+import 'package:keqv/keqv.dart';
 import 'package:test/test.dart';
 
-void main() {}
+void main() {
+  group("Read data", () {
+    test("from String directly", () {
+      const String testData = '''
+foo=bar
+i=1
+''';
+      Map<String, String?> decoded = keqv.decode(testData);
+      expect(decoded["foo"], equals("bar"));
+      expect(decoded["i"], "1");
+    });
+  });
+}
