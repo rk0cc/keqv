86bb7496449c4778e0b814f94aa6e0e712bad894
Cyrus Chan <cyruschan1212@gmail.com>
2024-04-09 22:54:04 +0800

    Prepare test

--- test/keqv_test.dart
+++ test/keqv_test.dart
@@ -2,19 +2,5 @@ import 'package:keqv/keqv.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group("Read data", () {
-    test("from String directly", () {
-      const String testData = '''
-foo=bar
-i=1
-bin=
-''';
-      Map<String, String?> decoded = keqv.decode(testData);
-      expect(decoded["foo"], equals("bar"));
-      expect(decoded["i"], equals("1"));
-      expect(decoded["baz"], isNull);
-      expect(decoded["bin"], isNull);
-      expect(decoded.containsKey("bin"), isTrue);
-    });
-  });
+  
 }
---
+++ test/str_escape_test.dart
@@ -0,0 +1,7 @@
+import 'package:test/test.dart';
+
+void main() {
+  group("String escape", () {
+    test("quoting", () {});
+  });
+}
