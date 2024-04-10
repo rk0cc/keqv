fc2784bbd15fe8adf89c7e1927fb4d64c5fa8cc7
Cyrus Chan <cyruschan1212@gmail.com>
2024-04-10 23:43:46 +0800

    Implement test

--- test/str_escape_test.dart
+++ test/str_escape_test.dart
@@ -1,7 +1,19 @@
+import 'package:keqv/keqv.dart' show EscapedCharCodec, Quoting;
 import 'package:test/test.dart';
 
 void main() {
+  const EscapedCharCodec singleQuote =
+          EscapedCharCodec(quoting: Quoting.singleQuote),
+      doubleQuote = EscapedCharCodec(quoting: Quoting.doubleQuote);
+
   group("String escape", () {
-    test("quoting", () {});
+    test("quoting", () {
+      expect(singleQuote.encode("123"), equals(r"'123'"));
+      expect(doubleQuote.encode("false"), equals(r'"false"'));
+    });
+    test("control character", () {
+      expect(doubleQuote.encode("foo\nbar"), equals(r'"foo\nbar"'));
+      expect(doubleQuote.encode("1\t2\r\n3"), equals(r'"1\t2\r\n3"'));
+    });
   });
 }
